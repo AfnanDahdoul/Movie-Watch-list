2b924f65eb75ca96602bd85339b25952adb80bad
AfnanDahdoul <afnandahduulll@gmail.com>
2026-07-07 20:04:44 +0300

    read and display the result as card

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const movieWatchListForm = document.forms['MovieWatchList'];
-const movies = [];
+let movies = [];
+
+if (localStorage.getItem("movies") != null){
+    movies = JSON.parse(localStorage.getItem("movies"));
+}
 movieWatchListForm.onsubmit = (e) => {
     e.preventDefault();
 
@@ -11,4 +15,21 @@ movieWatchListForm.onsubmit = (e) => {
 
     movies.push(movie);
     localStorage.setItem("movies", JSON.stringify(movies));
+    dispalyMovies();
 }
+
+const dispalyMovies = () =>{
+    const result = movies.map( (movie) => {
+        return `<div class="card" style="width: 18rem;">
+                    <img src="./assets/imgs/movie.png" class="card-img-top" alt="movie placeholder">
+                    <div class="card-body">
+                        <h5 class="card-title">${movie.name}</h5>
+                        <p class="card-text">${movie.genere}</p>
+                        <span href="#" class="btn btn-primary">${movie.rating}</span>
+                    </div>
+                </div>`;
+    })
+    document.querySelector(".card").innerHTML = result;
+}
+
+dispalyMovies();
